7ed9dae7216e04a873b0010698281139abf5cfb6
Kirill Shirinkin <fodojyko@gmail.com>
2015-03-11 22:52:17 +0100

    Step 1 - Project setup

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('ngmkdev', ['restangular', 'ui.router', 'ui.bootstrap'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ngmkdev')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
