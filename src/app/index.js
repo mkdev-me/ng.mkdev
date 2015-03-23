80488c3f721a0cbfe668cd62f10746b75710f88f
Kirill Shirinkin <fodojyko@gmail.com>
2015-03-23 16:42:31 +0100

    Minor cleanup before next step

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,6 @@
 
 angular.module('ngmkdev', ['restangular', 'ui.router', 'ui.bootstrap'])
   .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-
     $urlRouterProvider.otherwise('/');
   })
 ;
