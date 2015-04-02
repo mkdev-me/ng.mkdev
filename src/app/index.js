448daee54a90c20c9b888008afcd92b20a56a887
Kirill Shirinkin <fodojyko@gmail.com>
2015-04-02 12:22:25 +0200

    Step 4 - ui router

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,15 @@
 
 angular.module('ngmkdev', ['restangular', 'ui.router', 'ui.bootstrap'])
   .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('transactions', {
+      url: "/",
+      templateUrl: "app/main/transactions.html"
+    })
+    .state('settings', {
+      url: "/settings",
+      templateUrl: "app/settings/settings.html"
+    });
     $urlRouterProvider.otherwise('/');
-  })
+  });
 ;
